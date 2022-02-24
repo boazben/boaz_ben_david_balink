38e34de239a48df2f8c7431a127b6aa4bd018026
Boaz Ben-David <boaz.bdqwe@gmail.com>
2022-02-24 04:33:53 +0200

    localhost

--- Server/server.js
+++ Server/server.js
@@ -2,11 +2,8 @@ const express = require('express');
 const Router = require('./router');
 const cors = require('cors')
 const app = express()
-
-app.use(express.static('public'))
-app.use('*', express.static('public'))
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 
 Router(app)
 app.listen(4000, () => {console.log(`Server Is Runing In Port 4000!`);})
